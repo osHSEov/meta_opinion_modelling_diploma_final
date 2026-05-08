73807419441bfcc9ed2dcea697d906bc34198d38
Evtushenko Oleg <ovevtushenko@edu.hse.ru>
2026-05-08 13:01:47 +0300

    dataset fixes

--- services/ollama_client.py
+++ services/ollama_client.py
@@ -4,7 +4,7 @@ from .base_llm_client import BaseLLMClient
 
 class OllamaClient(BaseLLMClient):
     def __init__(self, config: dict):
-        self.model = config["name"]
+        self.model = config.get("model") or config.get("name")
         self.temperature = config["temperature"]
         self.seed = config["seed"]
         #self.num_ctx = config["num_ctx"]
